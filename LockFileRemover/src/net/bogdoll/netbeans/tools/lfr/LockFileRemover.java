7568c3a8f58597d5a8408399d6e17e43cd19e0a1
Dieter Bogdoll <bogdoll@gmail.com>
2013-07-25 10:44:11 +0200

    Using the api support module

--- LockFileRemover/src/net/bogdoll/netbeans/tools/lfr/LockFileRemover.java
+++ LockFileRemover/src/net/bogdoll/netbeans/tools/lfr/LockFileRemover.java
@@ -1,25 +1,22 @@
 package net.bogdoll.netbeans.tools.lfr;
 
-import java.io.IOException;
+import java.io.File;
 import java.util.logging.Logger;
-import javax.swing.Action;
 import static net.bogdoll.netbeans.tools.lfr.Bundle.*;
-import org.netbeans.api.project.Project;
+import org.netbeans.modules.apisupport.project.suite.SuiteProject;
 import org.openide.awt.ActionID;
 import org.openide.awt.ActionReference;
 import org.openide.awt.ActionReferences;
 import org.openide.awt.ActionRegistration;
-import org.openide.filesystems.FileObject;
 import org.openide.nodes.Node;
 import org.openide.util.*;
 import org.openide.util.NbBundle.Messages;
 import org.openide.util.actions.CookieAction;
-import org.openide.util.actions.SystemAction;
+import org.openide.windows.IOProvider;
+import org.openide.windows.InputOutput;
 
-@ActionID(category = "Tools",
-    id = "net.bogdoll.netbeans.tools.lfr.LockFileRemover")
-@ActionRegistration(iconBase = "net/bogdoll/netbeans/tools/lfr/draw-eraser.png",
-    displayName = "#CTL_LockFileRemover")
+@ActionID(category = "Tools", id = "net.bogdoll.netbeans.tools.lfr.LockFileRemover")
+@ActionRegistration(displayName = "#CTL_LockFileRemover", lazy = false, asynchronous = true)
 @ActionReferences({
     @ActionReference(path="Toolbars/Build", position = 250),
     @ActionReference(path="Projects/Actions")
@@ -38,9 +35,9 @@ public final class LockFileRemover extends CookieAction
         return "net/bogdoll/netbeans/tools/lfr/draw-eraser.png"; // NOI18N
     }
     
-    private boolean isRelevant(Project context) {
-        FileObject lockFile = getLockFile(context);
-        return lockFile!=null && lockFile.isData();
+    private boolean isRelevant(SuiteProject context) {
+        File testUserDirLockFile = context.getTestUserDirLockFile();
+        return testUserDirLockFile.exists() && testUserDirLockFile.isFile();
     }
 
     @Override
@@ -50,53 +47,33 @@ public final class LockFileRemover extends CookieAction
 
     @Override
     protected Class<?>[] cookieClasses() {
-        return new Class[]{Project.class};
+        return new Class[]{SuiteProject.class};
     }
 
     @Override
     protected boolean enable(Node[] activatedNodes) {
         for(Node n : activatedNodes) {
-            Project project = n.getLookup().lookup(Project.class);
-            if(isRelevant(project)) {
+            SuiteProject project = n.getLookup().lookup(SuiteProject.class);
+            if(project!=null && isRelevant(project)) {
                 return true;
             }
         }
         return false;
     }
     
-    private FileObject getLockFile(Project context) {
-        // LOG.info("Project: "+context);
-        if(context==null) return null;
-        
-        FileObject projectDir = context.getProjectDirectory();
-        // LOG.info("projectDir: "+projectDir);
-        if(projectDir==null) return null;
-        
-        FileObject build = projectDir.getFileObject("build");
-        // LOG.info("build: "+build);
-        if(build==null) return null;
-        
-        FileObject testDir = build.getFileObject("testuserdir");
-        // LOG.info("testDir: "+testDir);
-        if(testDir==null) return null;
-
-        return testDir.getFileObject("lock");
-    }
-
-    private void deleteLockFile(Project context) {
-        try {
-            FileObject lock = getLockFile(context);
-            lock.delete();
-        }
-        catch(IOException e) {
-            Exceptions.printStackTrace(e);
+    private void deleteLockFile(SuiteProject context) {
+        File testUserDirLockFile = context.getTestUserDirLockFile();
+        if(!testUserDirLockFile.delete()) {
+            InputOutput io = IOProvider.getDefault().getIO("Test user dir lock file remover", false);
+            io.getErr().println("Can't delete test user dir lock file: "+testUserDirLockFile.getAbsolutePath());
+            io.getErr().close();
         }
     }
 
     @Override
     protected void performAction(Node[] activatedNodes) {
         for(Node n : activatedNodes) {
-            Project project = n.getLookup().lookup(Project.class);
+            SuiteProject project = n.getLookup().lookup(SuiteProject.class);
             if(isRelevant(project)) {
                 deleteLockFile(project);
             }
